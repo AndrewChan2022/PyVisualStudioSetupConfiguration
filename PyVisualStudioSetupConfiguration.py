3e23482098960cb439d501dfee0509c644877d3b
AndrewChan <chenkaiphy@gmail.com>
2023-08-20 15:56:00 +0800

    copyright

--- PyVisualStudioSetupConfiguration.py
+++ PyVisualStudioSetupConfiguration.py
@@ -1,31 +1,3 @@
-#
-# Copyright 2023 Pixar
-#
-# Licensed under the Apache License, Version 2.0 (the "Apache License")
-# with the following modification; you may not use this file except in
-# compliance with the Apache License and the following modification to it:
-# Section 6. Trademarks. is deleted and replaced with:
-#
-# 6. Trademarks. This License does not grant permission to use the trade
-#    names, trademarks, service marks, or product names of the Licensor
-#    and its affiliates, except as required to comply with Section 4(c) of
-#    the License and to reproduce the content of the NOTICE file.
-#
-# You may obtain a copy of the Apache License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the Apache License with the above modification is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied. See the Apache License for the specific
-# language governing permissions and limitations under the Apache License.
-#
-
-# Utilities for finding Visual Studio installation information
-#
-# this file should keep as single file independent of others
-
 import os, subprocess, winreg, atexit, re
 import ctypes
 import ctypes.wintypes
@@ -762,7 +734,7 @@ class VSInstanceInfo:
 vsInstancesCache = None
 
 def GetAllVSInstanceInfo(needChipInfo = True, skipEWDK = True, ignoreCache = False):
-    """return list of VSInstanceInfo object
+    """return list of VSInstanceInfo object sorting with version desc
     needChipInfo: will enumerate package to get chip so a little slow
     skipEWDK: skip EWDK(Enterprise Windows Driver Kit)
     ignoreCache: not use cache stored by last call, only true for performance benchmark"""
